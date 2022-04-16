1eb6d9bf832270e1733e9c494566af657013d590
xuhui <xuhui3579@foxmail.com>
2022-04-16 18:06:59 +0800

    test1

---
+++ src/test/java/com/springboot/xuhui/XuhuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.xuhui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuhuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
